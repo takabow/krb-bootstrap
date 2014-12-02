020989899bc081a78f93adbe66f664f614ecb42c
Daisuke <poleon.kd@gmail.com>
2014-12-02 22:33:55 +0900

    make change for configuring with CM 5.1.0 and above

--- configure_krb5.sh
+++ configure_krb5.sh
@@ -184,8 +184,8 @@ create_kdc_database() {
   log DEBUG "Generating cloudera-scm/admin principal for Cloudera Manager"
 
   $KADMIN_LOCAL >/dev/null <<EOF
-  addprinc -randkey cloudera-scm/admin
-  xst -k cmf.keytab cloudera-scm/admin
+  addprinc -pw cloudera cloudera-scm/admin
+#  xst -k cmf.keytab cloudera-scm/admin
 EOF
 
   echo
@@ -257,7 +257,7 @@ install_krb_packages
 configure_kdc
 configure_krb_client
 create_kdc_database
-configure_cm_files
+#configure_cm_files
 start_services
 display_next_steps
 
