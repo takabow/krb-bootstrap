ad987532052141178d4107362eb7246f2b70a3e6
Daisuke <poleon.kd@gmail.com>
2015-07-08 23:52:36 +0900

    force to install/use jq to parse the json file, added display_next_steps too

--- enabling_krb_using_cm.sh
+++ enabling_krb_using_cm.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright Cloudera 2015
 
 # https://groups.google.com/a/cloudera.org/forum/#!topic/scm-users/ChGln8DekBM
 
@@ -6,51 +20,51 @@ CMNODE=localhost
 TARGET="$@"
 BASE=http://$CMNODE:7180/api/v8
 CLUSTER=$(curl -X GET -u "admin:admin" -i $BASE/clusters | grep '"name"' | awk -F'"' '{print $4}')
+services_json=`curl -u admin:admin "$BASE/clusters/$CLUSTER/services" | jq '[.items[]|{name, type}]'`
+num_services=`echo $services_json | jq 'length'`
 
-hdfs=$(curl -X GET -u "admin:admin" -i $BASE/clusters/$CLUSTER/services | grep '"displayName"' | grep -i hdfs | awk -F'"' '{print $4}')
-zookeeper=$(curl -X GET -u "admin:admin" -i $BASE/clusters/$CLUSTER/services | grep '"displayName"' | grep -i zookeeper | awk -F'"' '{print $4}')
-yarn=$(curl -X GET -u "admin:admin" -i $BASE/clusters/$CLUSTER/services | grep '"displayName"' | grep -i yarn | awk -F'"' '{print $4}')
-mr1=$(curl -X GET -u "admin:admin" -i $BASE/clusters/$CLUSTER/services | grep '"displayName"' | grep -i mapreduce | awk -F'"' '{print $4}')
-hbase=$(curl -X GET -u "admin:admin" -i $BASE/clusters/$CLUSTER/services | grep '"displayName"' | grep -i hbase | awk -F'"' '{print $4}')
+installjq(){
+    curl -o /usr/local/bin/jq http://stedolan.github.io/jq/download/linux64/jq && sudo chmod +x /usr/local/bin/jq
+}
 
 zk(){
     curl -s -X PUT -H 'Content-type:application/json' \
 	-d '{"items":[{"name":"enableSecurity","value":"true"}]}' \
 	-u admin:admin \
-	$BASE/clusters/$CLUSTER/services/$zookeeper/config
+	$BASE/clusters/$CLUSTER/services/$1/config
 }
 
 hdfs(){
     curl -s -X PUT -H 'Content-type:application/json' \
 	-d '{"items":[{"name":"hadoop_security_authentication","value":"kerberos"},{"name":"hadoop_security_authorization","value":"true"}]}' \
 	-u admin:admin \
-	$BASE/clusters/$CLUSTER/services/$hdfs/config
+	$BASE/clusters/$CLUSTER/services/$1/config
 
     curl -s -X PUT -H 'Content-type:application/json' \
 	-d '{"items":[{"name":"dfs_datanode_http_port","value":"1006"}, 
 {"name":"dfs_datanode_port","value":"1004"}, 
 {"name":"dfs_datanode_data_dir_perm","value":"700"}]}' \
 	-u admin:admin \
-	$BASE/clusters/$CLUSTER/services/$hdfs/roleConfigGroups/$hdfs-DATANODE-BASE/config
+	$BASE/clusters/$CLUSTER/services/$1/roleConfigGroups/$1-DATANODE-BASE/config
 }
 
 mr(){
     [ -n "mr1" ]; curl -s -X PUT -H 'Content-type:application/json' \
 	-d '{"items":[{"name":"taskcontroller_min_user_id","value":"0"}]}' \
 	-u admin:admin \
-	$BASE/clusters/$CLUSTER/services/$mr1/roleConfigGroups/$mr1-TASKTRACKER-BASE/config
+	$BASE/clusters/$CLUSTER/services/$1/roleConfigGroups/$1-TASKTRACKER-BASE/config
 
     [ -n "yarn" ]; curl -s -X PUT -H 'Content-type:application/json' \
 	-d '{"items":[{"name":"container_executor_min_user_id","value":"0"}]}' \
 	-u admin:admin \
-	$BASE/clusters/$CLUSTER/services/$yarn/roleConfigGroups/$yarn-NODEMANAGER-BASE/config
+	$BASE/clusters/$CLUSTER/services/$1/roleConfigGroups/$1-NODEMANAGER-BASE/config
 }
 
 hbase(){
     [ -n "hbase" ]; curl -s -X PUT -H 'Content-type:application/json' \
 	-d '{"items":[{"name":"hbase_security_authentication","value":"kerberos"},{"name":"hbase_security_authorization","value":"true"}]}' \
 	-u admin:admin \
-	$BASE/clusters/$CLUSTER/services/$hbase/config
+	$BASE/clusters/$CLUSTER/services/$1/config
 }
 
 krb(){
@@ -69,25 +83,40 @@ other(){
 	$BASE/cm/commands/importAdminCredentials
 }
 
-restart(){
-    curl -X POST -u "admin:admin" -i \
-	-H "Content-Type:application/json" \
-	-d '{"redeployClientConfiguration": true, "restartOnlyStaleServices": null}' \
-    $BASE/clusters/$CLUSTER/commands/restart
-#    curl -X POST -u admin:admin "$BASE/cm/service/commands/restart"
+display_next_steps() {
+  echo "*** NEXT ***"
+  echo
+  echo "As the configurations are being updated, stop the entire services,"
+  echo "deploy Kerberos client configuration, deploy cluster client configuration, "
+  echo "and start the cluster."
 }
 
-zk
-hdfs
-mr
-hbase
+installjq
+
+index=0
+while [ $index -lt $num_services ]; do
+    dictionary=`echo $services_json | jq ".[$index]"`
+    name=`echo $dictionary | jq -r '.name'`
+    type=`echo $dictionary | jq -r '.type'`
+
+    case $type in
+        HBASE)
+            hbase $name
+            ;;
+        HDFS)
+            hdfs $name
+            ;;
+        YARN)
+            mr $name
+            ;;
+        ZOOKEEPER)
+            zk $name
+            ;;
+    esac
+
+    let index=index+1
+done
+
 krb
 other
-restart
-
-
-
-
-
-
-
+display_next_steps
